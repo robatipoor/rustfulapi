f9e17c9f10bf9dd60ba668da2ad1b63944c571d0
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-01-07 14:00:11 +1100

    feat(server::run): add graceful shutdown implementation

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -2,6 +2,9 @@ use self::state::AppState;
 use crate::configure::AppConfig;
 use crate::error::AppResult;
 use crate::router::create_router_app;
+use tower_http::timeout::TimeoutLayer;
+use tower_http::trace::TraceLayer;
+use tracing::info;
 pub mod state;
 pub mod worker;
 
@@ -20,8 +23,46 @@ impl AppServer {
   }
 
   pub async fn run(self) -> AppResult<()> {
-    let router = create_router_app(self.state);
-    axum::serve(self.tcp, router).await?;
+    let shutdown = self.grace_shutdown_time();
+    let router = create_router_app(self.state)
+      .layer(TraceLayer::new_for_http()) // Visibility of the request and response, change as needed.
+      .layer(TimeoutLayer::new(shutdown)); // Graceful shutdown for hosting services requries N time to complete the request before shutting down.
+
+    axum::serve(self.tcp, router)
+      .with_graceful_shutdown(shutdown_signal())
+      .await?;
     Ok(())
+  }
+
+  fn grace_shutdown_time(&self) -> std::time::Duration {
+    std::time::Duration::from_secs(self.state.config.server.grace_shutdown_secs as u64)
+  }
+}
+
+async fn shutdown_signal() {
+  let ctrl_c = async {
+    tokio::signal::ctrl_c()
+      .await
+      .expect("Failed to install CTRL+C signal handler");
+  };
+
+  #[cfg(unix)]
+  let terminate = async {
+    tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+      .expect("Failed to install SIGTERM signal handler")
+      .recv()
+      .await;
+  };
+
+  #[cfg(not(unix))]
+  let terminate = std::future::pending::<()>();
+
+  tokio::select! {
+      _ = ctrl_c => {
+          info!("Received Ctrl-C signal. Shutting down...");
+      },
+      _ = terminate => {
+          info!("Received SIGTERM signal. Shutting down...");
+      },
   }
 }
